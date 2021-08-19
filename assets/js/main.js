6d330ac8e2abe11656b842b0685de71599ebbc27
Ken Karlson <kenkarlson@mail.ru>
2021-08-19 20:23:31 +0300

    on active buttons and highlighting elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,42 @@
 $(document).ready(function () {
   let currentFloor = 0;
-  $(".home-image path").on('click', function () {
-    currentFloor = ($(this).attr("data-floor"));
+  let floorPath = $('.home-image path');
+  let counterUp = $('.counter-up'); /* кнопка вверх */
+  let counterDown = $('.counter-down'); /* кнопка вниз */
+
+  //При наведении мышки на этаж
+  floorPath.on('mouseover', function () {
+    floorPath.removeClass('current-floor');
+    currentFloor = ($(this).attr('data-floor'));
     console.log(currentFloor);
+    $('.counter').text(currentFloor);
+  });
+
+  //при нажатии кнопки вверх
+  counterUp.on('click', function () {
+    if (currentFloor < 18) {
+      currentFloor++;
+      usCurrentFloor = currentFloor.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+      });
+      $('.counter').text(usCurrentFloor);
+      floorPath.removeClass('current-floor');
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+    }
+  });
+
+  //при нажатии кнопки вниз
+  counterDown.on('click', function () {
+    if (currentFloor > 02) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+      });
+      $('.counter').text(usCurrentFloor);
+      floorPath.removeClass('current-floor');
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+    }
   });
 });
